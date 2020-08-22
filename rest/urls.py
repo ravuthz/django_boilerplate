f1b3d340e2a5086e0d01bfd1755b91a37249e1f7
Ravuthz <ravuthz@gmail.com>
2020-08-22 13:15:24 +0700

    Add Swagger & Redoc for Django Rest API

--- rest/urls.py
+++ rest/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path, re_path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import routers, permissions
 
 from .views import PermissionViewSet, GroupViewSet, UserViewSet
 
@@ -7,6 +9,19 @@ router = routers.DefaultRouter()
 router.register(r'permissions', PermissionViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(r'users', UserViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Boilerplate API",
+        default_version='v1',
+        description="Boilerplate API version 1.0.0",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="adminz@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     # AUTH 0 authentication
@@ -20,4 +35,8 @@ urlpatterns = [
 
     # Rest Framework Auth support Browsable API
     path('', include('rest_framework.urls', namespace='rest_framework')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
 ]
